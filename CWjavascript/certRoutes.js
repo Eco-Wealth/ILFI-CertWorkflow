7c507a24d3fa3a384fcfcf3982723c90cf87037a
brawlaphant <35781613+brawlaphant@users.noreply.github.com>
2023-10-06 19:26:16 -0700

    Create certRoutes.js

---
+++ CWjavascript/certRoutes.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+// Define your certification-related routes here
+// For example, route for project registration, audits, appeals, etc.
+
+module.exports = router;
